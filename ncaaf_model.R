1836502fb1bc183b2ebcb33a81117366b65d57f0
Eric Peterson <EricPPeterson@users.noreply.github.com>
2024-05-25 21:30:06 +1000

    Update ncaaf_model.R

--- ncaaf_model.R
+++ ncaaf_model.R
@@ -144,7 +144,8 @@ down_efficiency <- function(df, play_type, pos_def, dwn){
                      total_wpa = sum(wpa),
                      total_success = sum(success),
                      total_downs = n(),
-                     success_rate = total_success / total_downs)
+                     success_rate = total_success / total_downs,
+                     avg_ytg_log = mean(log_ydstogo))
    x <- ifelse(play_type == 1,'pass','rush')  
    colnames(out)[3:7] <- paste(colnames(out)[3:7],x,dwn,sep = '_') 
    return(out)
@@ -173,3 +174,84 @@ off_total_efficiency <- list(off_total_efficiency,off_pass_eff_1,off_pass_eff_2,
 def_total_efficiency <- list(def_total_efficiency,def_pass_eff_1,def_pass_eff_2,def_pass_eff_3,
                              def_rush_eff_1, def_rush_eff_2, def_rush_eff_3) %>% 
   reduce(left_join, by = c('game_id', 'def_pos_team'))
+##################################################################################################################
+#touchdown points
+##################################################################################################################
+td_pts <- function(df,pos_def){
+  out <- df %>%
+    dplyr::group_by(game_id, !!sym(pos_def)) %>%
+    dplyr::summarise(td_pts = sum(touchdown) * 7)
+  return(out)
+}
+
+off_tds <- td_pts(pbp_clean, 'pos_team')
+def_tds <- td_pts(pbp_clean, 'def_pos_team')
+##################################################################################################################
+#expected FG points
+##################################################################################################################
+exp_fg <- function(df, pos_def){
+  out <- df %>%
+    dplyr::group_by(game_id, !!sym(pos_def)) %>%
+    dplyr::filter(fg_inds == 1) %>%
+    dplyr::summarise(fgs_pts = sum(3 * fg_make_prob))
+  out$fgs_pts <- ifelse(is.na(out$fgs_pts),0,out$fgs_pts)
+  return(out)
+}
+
+off_fg_pts <- exp_fg(pbp_clean, 'pos_team')
+def_fg_pts <- exp_fg(pbp_clean, 'def_pos_team')
+##################################################################################################################
+#total points offense
+##################################################################################################################
+off_total_pts <- left_join(off_tds, off_fg_pts, by = c('game_id', 'pos_team'))
+off_total_pts$fgs_pts <- ifelse(is.na(off_total_pts$fgs_pts), 0, off_total_pts$fgs_pts)
+off_total_pts <- off_total_pts %>%
+  dplyr::mutate(total_pts = td_pts + fgs_pts)
+##################################################################################################################
+#join pts to offensive efficiency
+##################################################################################################################
+off_total_efficiency$total_pts <- lookup(off_total_efficiency$game_id, off_total_pts$game_id, off_total_pts$total_pts)
+off_total_efficiency_NA <- na.omit(off_total_efficiency) %>%
+  ungroup()
+##################################################################################################################
+#linear model
+##################################################################################################################
+library(tidyverse)
+library(tidymodels)
+##################################################################################################################
+#split data
+##################################################################################################################
+data_split_lm <- initial_split(off_total_efficiency, prop = 4/5)
+train_lm <- training(data_split_lm)
+test_lm <- testing(data_split_lm)
+##################################################################################################################
+#create recipes for different linear models
+##################################################################################################################
+lm_start <- recipe(total_pts~., data = train_lm) %>%
+  update_role(game_id, pos_team, new_role = 'id_variable') %>%
+  step_impute_mean()
+##################################################################################################################
+#select model types / select engine
+##################################################################################################################
+lm_mod <- linear_reg() %>%
+  set_engine('glm')
+##################################################################################################################
+# set workflows
+##################################################################################################################
+off_start_wflow <- workflow() %>%
+  add_model(lm_mod) %>%
+  add_recipe(lm_start)
+##################################################################################################################
+#fit models
+##################################################################################################################
+off_start_fit <- off_start_wflow %>%
+  fit(data = train_lm)
+model_summary <- off_start_fit %>%
+  extract_fit_parsnip() %>%
+  tidy()
+##################################################################################################################
+#make predictions
+##################################################################################################################
+start_preds <- predict(off_start_fit, test_lm)
+bind_preds <- bind_cols(start_preds, test_lm %>% select(total_pts))
+rmse(total_pts, .pred, data = bind_preds)
